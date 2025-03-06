176b04e2fce42c9feb4f5c994a0221b68457c351
mikel souza <mikel.mikel21@gmail.com>
2025-03-06 15:47:48 -0300

    trabalhando com operadores e seus conceitos

---
+++ tipos-variaveis-java/src/Operadores.java
@@ -0,0 +1,28 @@
+public class Operadores {
+    
+    public static void main(String[] args) {
+    
+       String nomeCompleto = "OPERADORES-" + "JAVA";
+       
+       System.out.println(nomeCompleto);
+
+       int numero = 5;
+
+       numero = - numero;
+
+       System.out.println(numero);
+
+       numero = numero * -1;
+
+       System.out.println(numero);
+
+       System.out.println(++numero);
+       
+    boolean variavel = true;
+
+    variavel = !variavel;
+    
+    System.out.println(variavel);
+
+    }
+}

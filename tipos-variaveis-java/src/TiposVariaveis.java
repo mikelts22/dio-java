11c0617d63047af826e65a7d87da37aab56b5f83
mikel souza <mikel.mikel21@gmail.com>
2025-03-06 14:41:09 -0300

    alteraçao no projeto

--- tipos-variaveis-java/src/TiposVariaveis.java
+++ tipos-variaveis-java/src/TiposVariaveis.java
@@ -1,5 +1,18 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        String meuNome ="MIKEL TAMBORINI";
+
+        double salarioMinimo = 2500;
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+
+        final int numero = 5;
+
+        System.out.print(numero);
+
+        final double VALOR_DE_PI = 3.14;
+
     }
 }
